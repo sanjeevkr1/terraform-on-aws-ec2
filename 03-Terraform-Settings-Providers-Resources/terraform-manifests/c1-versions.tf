da78d45b57e8093de64d31060b4c002de0bfc595
Sanjeev Kumar <sanjeev.kumar13@vodafone.com>
2023-10-03 00:21:14 +0530

    added own files and disabled original files

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,20 +1,10 @@
-# Terraform Block
 terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.5.6" // this means any version above x.x.6 is allowed to use (only minor version)
   required_providers {
+    //https://registry.terraform.io/providers/hashicorp/aws/latest/docs
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
-  } 
-}  
-# Provider Block
-provider "aws" {
-  region = "us-east-1"
+  }
 }
-
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
-
